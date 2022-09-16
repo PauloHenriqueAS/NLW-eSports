11ab990c8d79ff2e06ddddd989540645f44f0fcf
Paulo Henrique <paulohalves_00@hotmail.com>
2022-09-15 21:22:43 -0300

    [ADD] first commit

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+//criar a aplicação
+const app = express();
+//primeira rota
+//parametros: rota a ser acessada e método que será executado
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, nome: "Materia 1" },
+        { id: 2, nome: "Materia 2" },
+        { id: 3, nome: "Materia 3" }
+    ]);
+});
+app.listen(3333);
---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/main.css'; //Importação do tailwind
+
+//JSX -> JavaScript + XML(HTML)
+//tag com letra maiuscula = Componente personalidado no react
+function App() {
+  return (
+    <div className='w-8 h-8 bg-violet-500'>
+
+    </div>
+  )
+}
+
+export default App
